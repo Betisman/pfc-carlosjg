d44c009d37d91b0cfbe43b7d840d895449bdfdfb
Carlos Jimenez <betisman@gmail.com>
2016-12-06 13:36:17 +0100

    parece haber un problema al eleer el raw_json. A veces lo lee como string y otras llega como objeto. Cuando esto ultimo pasa, habia problemas y no funcionaba la cabina de voto. Lo que hacemos ahora es controlarlo. Cuando llega como string, parseamos, cuando llega como objeto, dejamos que fluya

--- src/helios-server-eps/heliosbooth/js/jscrypto/helios.js
+++ src/helios-server-eps/heliosbooth/js/jscrypto/helios.js
@@ -127,7 +127,9 @@ HELIOS.Election.fromJSONString = function(raw_json) {
   //var json_object = JSON.parse(raw_json);
   // Al actualizar jquery, no es necesario parsear el JSON
   //var json_object = raw_json;
-  var json_object = JSON.parse(raw_json);
+  var json_object = raw_json;
+  if (typeof raw_json === 'string')
+	json_object = JSON.parse(raw_json);
   
   // let's hash the raw_json
   var election = HELIOS.Election.fromJSONObject(json_object);
